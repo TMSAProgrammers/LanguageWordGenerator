d55de22c2274321a89e4277c2d9cdfc221107d38
Lukas\Laptop2 <lukasvantilburg@gmail.com>
2018-02-08 18:01:27 -0500

    Added basis of a GUI

--- src/com/company/GUIHandler.java
+++ src/com/company/GUIHandler.java
@@ -1,5 +1,37 @@
 package com.company;
 
-public class GUIHandler {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class GUIHandler extends JFrame implements ActionListener {
+
+    //Fields
+    String mainOutput;
+
+    //Freqency maps
+    Frequency consonants;
+    Frequency vowels;
+    Frequency wordStarts;
+    Frequency wordEnds;
+    Frequency forbiddens;
+
+
+    //Constructor
+    public GUIHandler() {
+        initializeWindow();
+    }
+
+    private void initializeWindow() {
+        setTitle("Language Word Generator");
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JTextField) {
+
+        }
+    }
 }
